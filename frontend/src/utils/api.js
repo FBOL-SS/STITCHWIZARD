956bdf5edb60daa8d55b39a092877f9d334596b9
FBOL-SS <67643371+FBOL-SS@users.noreply.github.com>
2025-10-14 14:39:34 -0600

    Create api.js

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || '/api',
+});
+
+api.defaults.headers.common['Authorization'] = 'Bearer dummy_token';
+
+export default api;
