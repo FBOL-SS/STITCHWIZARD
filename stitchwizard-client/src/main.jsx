e2995d4fbbfdd01a60007b19b5690491a4586a90
FBOL-SS <67643371+FBOL-SS@users.noreply.github.com>
2025-10-14 17:37:28 -0600

    Update main.jsx

--- stitchwizard-client/src/main.jsx
+++ stitchwizard-client/src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // si aplica
+import './index.css'; // opcional
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
