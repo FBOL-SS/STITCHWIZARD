6ca120044f2c284eba9d24700db2d59ee14dc25c
FBOL-SS <67643371+FBOL-SS@users.noreply.github.com>
2025-10-14 17:43:39 -0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Asegúrate de que exista este archivo o elimina esta línea si no lo usas
+import './index.css'; // Asegúrate de que exista este archivo o bórralo si no lo usas
 
-// Render principal
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
